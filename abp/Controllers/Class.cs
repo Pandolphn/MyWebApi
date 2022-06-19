4ef374acfd3ba0bf6f9641e14fc748e3bfbec068
Pandolphn <982370574@qq.com>
2022-06-20 01:13:21 +0800

    2

---
+++ abp/Controllers/Class.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Applictaion.User;
+using Abp.Applictaion.User.Dto;
+using Abp.Domian.UserInfo;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Utility.AOP;
+
+namespace Abp.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+      [TypeFilter(typeof(APIExceptionFilterAttribute))]
+    public class HealthController : ControllerBase
+    {
+        private IUserAppService  _userAppService;
+        public HealthController(IUserAppService  userAppService)
+        {
+            this._userAppService = userAppService;
+        }
+        [HttpGet]
+        public async Task<UserDto> Get()
+        {
+            throw new NotImplementedException();
+            return await _userAppService.Get("23414", "123");
+           
+        }
+
+        
+    }
+}
