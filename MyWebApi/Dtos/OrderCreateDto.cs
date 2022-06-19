54701b2d7679bb5853c111fd177e21d2e0da1c61
Pandolphn <982370574@qq.com>
2022-06-20 01:11:22 +0800

    1
    清理

--- MyWebApi/Dtos/OrderCreateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyWebApi.Dtos
-{
-    public class OrderCreateDto
-    {
-        public string id { get; set; }  
-        public string orderid { get; set; }
-        public DateTime orderdate { get; set; }
-    }
-}
